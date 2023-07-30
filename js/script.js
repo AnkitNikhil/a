13223806cca4057c1875426eb65ace518626509b
Ankit Nikhil <ankitnikhil07@gmail.com>
2023-07-30 22:28:17 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,129 +13,6 @@ $(document).ready(function () {
   });
 });
 
-// Form submission
-$(document).ready(function () {
-  $('#contact-form').on('submit', function (event) {
-    event.preventDefault();
-
-    // Perform form validation here
-
-    // Send form data to server using AJAX or any other method
-
-    // Display success message or handle errors
-  });
-});
-// Particle effect background
-particlesJS('particles-js', {
-  "particles": {
-    "number": {
-      "value": 80,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
-
 // Animate sections on scroll
 $(document).ready(function () {
   $(window).scroll(function () {
@@ -148,30 +25,60 @@ $(document).ready(function () {
     });
   });
 });
+
+// JavaScript Animation
+window.addEventListener('scroll', animateAboutSection);
+
+function animateAboutSection() {
+  const aboutContainer = document.querySelector('.about-container');
+  if (!aboutContainer) return; // Check if the element exists, if not, return early
+
+  const aboutContainerPosition = aboutContainer.getBoundingClientRect().top;
+  const screenPosition = window.innerHeight / 1.3;
+
+  if (aboutContainerPosition < screenPosition) {
+    aboutContainer.classList.add('show');
+  }
+}
+
+
+function fadeInAboutContent() {
+  const aboutContent = document.querySelector('.about-content');
+  const rect = aboutContent.getBoundingClientRect();
+  const windowHeight = window.innerHeight;
+
+  if (rect.top <= windowHeight * 0.8) {
+    aboutContent.classList.add('fade-in');
+  }
+}
+
+window.addEventListener('scroll', fadeInAboutContent);
+window.addEventListener('load', fadeInAboutContent);
+
+// Add "animated" class to each letter of the text
+document.addEventListener("DOMContentLoaded", function () {
+  var letters = document.querySelectorAll(".navbar-brand .animated-text");
+  letters.forEach(function (letter) {
+    letter.innerHTML = letter.textContent.replace(/\S/g, "<span class='animated-letter'>$&</span>");
+  });
+});
+
 // Form submission
 $(document).ready(function () {
   $('#contact-form').on('submit', function (event) {
     event.preventDefault();
 
     // Perform form validation
-    var firstName = $('#first-name').val();
-    var lastName = $('#last-name').val();
+    var name = $('#name').val();
     var email = $('#email').val();
     var message = $('#message').val();
     var isValid = true;
 
-    if (firstName.trim() === '') {
-      $('#first-name-error').text('Please enter your first name');
+    if (name.trim() === '') {
+      $('#name-error').text('Please enter your name');
       isValid = false;
     } else {
-      $('#first-name-error').text('');
-    }
-
-    if (lastName.trim() === '') {
-      $('#last-name-error').text('Please enter your last name');
-      isValid = false;
-    } else {
-      $('#last-name-error').text('');
+      $('#name-error').text('');
     }
 
     if (email.trim() === '') {
@@ -192,11 +99,26 @@ $(document).ready(function () {
     }
 
     if (isValid) {
-      // Simulate form submission
-      setTimeout(function () {
-        $('#contact-form')[0].reset();
-        $('#success-message').text('Your message has been sent successfully!');
-      }, 1000);
+      // Submit the form using Formspree
+      $.ajax({
+        url: 'https://formspree.io/f/mleyggko',
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: 'json',
+        beforeSend: function () {
+          // Add loading animation or disable the submit button if desired
+          $('#success-message').text('Sending...');
+        },
+        success: function (data) {
+          // Clear the form and display success message
+          $('#contact-form')[0].reset();
+          $('#success-message').text('Your message has been sent successfully!');
+        },
+        error: function (error) {
+          // Display error message if the form submission fails
+          $('#success-message').text('There was an error sending your message. Please try again later.');
+        }
+      });
     }
   });
 
@@ -209,8 +131,122 @@ $(document).ready(function () {
 // JavaScript Animation
 window.addEventListener('scroll', animateAboutSection);
 
+function newFunction() {
+  particlesJS('particles-js', {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
+}
+
 function animateAboutSection() {
   const aboutContainer = document.querySelector('.about-container');
+  if (!aboutContainer) return; // Check if the element exists, if not, return early
+
   const aboutContainerPosition = aboutContainer.getBoundingClientRect().top;
   const screenPosition = window.innerHeight / 1.3;
 
@@ -218,6 +254,7 @@ function animateAboutSection() {
     aboutContainer.classList.add('show');
   }
 }
+
 // JavaScript
 const aboutSection = document.querySelector('#about');
 
@@ -235,9 +272,75 @@ window.addEventListener('scroll', fadeInAboutContent);
 window.addEventListener('load', fadeInAboutContent);
 
 // Add "animated" class to each letter of the text
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var letters = document.querySelectorAll(".navbar-brand .animated-text");
-  letters.forEach(function(letter) {
+  letters.forEach(function (letter) {
     letter.innerHTML = letter.textContent.replace(/\S/g, "<span class='animated-letter'>$&</span>");
   });
+});
+/******************Contact Form********************/
+// Form submission
+$(document).ready(function () {
+  $('#contact-form').on('submit', function (event) {
+    event.preventDefault();
+
+    // Perform form validation
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var message = $('#message').val();
+    var isValid = true;
+
+    if (name.trim() === '') {
+      $('#name-error').text('Please enter your name');
+      isValid = false;
+    } else {
+      $('#name-error').text('');
+    }
+
+    if (email.trim() === '') {
+      $('#email-error').text('Please enter your email');
+      isValid = false;
+    } else if (!isValidEmail(email)) {
+      $('#email-error').text('Please enter a valid email');
+      isValid = false;
+    } else {
+      $('#email-error').text('');
+    }
+
+    if (message.trim() === '') {
+      $('#message-error').text('Please enter your message');
+      isValid = false;
+    } else {
+      $('#message-error').text('');
+    }
+
+    if (isValid) {
+      // Submit the form using Formspree
+      $.ajax({
+        url: 'https://formspree.io/f/mleyggko',
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: 'json',
+        beforeSend: function () {
+          // Add loading animation or disable the submit button if desired
+          $('#success-message').text('Sending...');
+        },
+        success: function (data) {
+          // Clear the form and display success message
+          $('#contact-form')[0].reset();
+          $('#success-message').text('Your message has been sent successfully!');
+        },
+        error: function (error) {
+          // Display error message if the form submission fails
+          $('#success-message').text('There was an error sending your message. Please try again later.');
+        }
+      });
+    }
+  });
+
+  // Validate email format
+  function isValidEmail(email) {
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
 });
